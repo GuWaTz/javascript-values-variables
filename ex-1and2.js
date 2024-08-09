aed164213243c19782c1532538d5ceaa337bcb51
Pollawat Hlunkeaw <pollawathlunkeaw@iMac-khxng-Pollawat.local>
2024-08-09 14:24:45 +0700

    ex all

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,11 @@
-//Exercise #1: Variable Creation
-//Start coding here
+let name = Karter;
+let surname = Anderson;
+let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai";
 
-//Exercise #2: Variable Reassignment
-//Start coding here
+const restaurant = "Chef's Table by Chef James";
+const total = 15000;
+const totalrestaurant = 1500;
+
+address = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641";
+total = 5000;
+totalrestaurant = 2500;
